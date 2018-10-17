c2ac2b92a75bf16de71802e168c819dbb47c8f71
Tulir Asokan <tulir@maunium.net>
2018-10-17 20:17:57 +0300

    Update imports

--- factorial.py
+++ factorial.py
@@ -13,8 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from maubot import Plugin, CommandSpec, PassiveCommand
-from mautrix.types import MessageEvent
+from maubot import Plugin, CommandSpec, PassiveCommand, MessageEvent
 
 COMMAND_FACTORIAL = "xyz.maubot.factorial"
 
